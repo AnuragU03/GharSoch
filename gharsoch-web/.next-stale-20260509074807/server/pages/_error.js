086d3d63b367ecaf3083651488a8cf2693d034d9
AnuragU03 <anuragugargol@gmail.com>
2026-05-09 09:55:49 +0530

    chore: purge .next-stale build caches and logs from git tracking; fix .gitignore for root repo structure

--- gharsoch-web/.next-stale-20260509074807/server/pages/_error.js
+++
@@ -1,5 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/pages/_error.js";
-const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_46b4c3._.js");
-runtime.loadChunk("server/chunks/ssr/pages_e13800._.js");
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/pages/_error.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/pages/_document.tsx [ssr] (ecmascript)\", INNER_APP => \"[project]/node_modules/next/app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
